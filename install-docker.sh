ba3669a893f0023aa8356fd7c10897792d3fff14
Timar <timar.jawel@gmail.com>
2018-08-09 20:50:34 +0200

    add install docker script

---
+++ install-docker.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# install older versions
+sudo apt-get remove -y docker docker-engine docker.io
+
+# install dependencies
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# add GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# add repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# update repositories
+sudo apt-get update
+
+# install docker
+sudo apt-get install -y docker-ce
+
+# add docker group
+sudo groupadd docker
+
+# add current user to docker group
+sudo usermod -aG docker $USER
+
+# test if docker works correctly
+docker run hello-world
