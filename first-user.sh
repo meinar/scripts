4c028e4dbce5f181954e51476fc567fb3104914c
Timar <timar.jawel@gmail.com>
2018-08-09 21:32:51 +0200

    updates

--- first-user.sh
+++ first-user.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-echo "Asername:"
+echo "Username:"
 read newUser
 
 adduser $newUser
 usermod -aG sudo $newUser
 
-cp ~/.ssh /home/$newUser
+cp -r ~/.ssh /home/$newUser
 chown -R $newUser:$newUser /home/$newUser/.ssh
 
 echo "User '$newUser' successfully created"
